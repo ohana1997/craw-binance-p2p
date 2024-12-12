5ad6bf80673d890c9a69dd3e9a68c4d0fb755aff
Nguyen Nam <namnd@payme.vn>
2024-12-12 09:11:39 +0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # craw-binance-p2p
+
+Sample usage
+
+   
+
+```javascript
+     const { getTransactions } = require('./index');
+     const result = await getTransactions({ fiat: 'USD', tradeType: 'SELL' })
+
+    // response
+    {
+        success: true,
+        data: [
+                {
+                    adv: {
+                        tradeType: 'BUY',
+                        fiatUnit: 'USD',
+                        price: '1.040',
+                        surplusAmount: '1945930.78',
+                        tradableQuantity: '1945930.78',
+                        minSingleTransAmount: '2000',
+                        maxSingleTransAmount: '100000',
+                        tradeMethods: [
+                            'Array'
+                        ],
+                        fiatSymbol: '$'
+                    },
+                    advertiser: {
+                        nickName: 'TAKE_AND_GIVE',
+                        monthOrderCount: 109,
+                        monthFinishRate: 0.88,
+                        positiveRate: 0.97750913
+	                }
+                }
+        ],
+        message: 'success'
+  
+    }
+```
+
+
---
+++ index.js
@@ -0,0 +1,80 @@
+const axios = require('axios');
+
+const PRIVATE = {
+  apiUrl: 'https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}
+
+function getKeysOnDemand(data, keysToKeep) {
+  if (Array.isArray(data)) {
+      return data.map(item => getKeysOnDemand(item, keysToKeep)); // Process arrays
+  } else if (typeof data === "object" && data !== null) {
+      const newData = {};
+      for (const key of keysToKeep) {
+          if (key in data) {
+              newData[key] = getKeysOnDemand(data[key], keysToKeep); // Recursively process nested objects
+          }
+      }
+      return newData;
+  }
+  return data; // Return value if not an object or array
+  }
+
+
+async function getTransactions(options) {
+  const defaultPayload = {
+    fiat: 'VND',
+    page: 1,
+    rows: 5,
+    tradeType: 'BUY',
+    asset: 'USDT',
+    countries: [],
+    proMerchantAds: false,
+    shieldMerchantAds: false,
+    filterType: 'tradable',
+    periods: [],
+    additionalKycVerifyFilter: 0,
+    publisherType: 'merchant',
+    payTypes: [],
+    classifies: ['mass', 'profession', 'fiat_trade'],
+  };
+
+  // Merge default payload with user-provided options
+  const payload = { ...defaultPayload, ...options };
+
+  try {
+    const { data: { code, message, data, success } } = await axios.post(PRIVATE.apiUrl, payload, { headers: PRIVATE.headers });
+    if (code !== '000000' || success === false ) {
+      return {
+          success: false,
+          data: null,
+          message
+      }
+    }
+
+      const keysToKeep  = ['adv', 'advertiser', 'tradeType', 'fiatUnit', 'price', 'surplusAmount', 'tradableQuantity', 'minSingleTransAmount', 'maxSingleTransAmount',
+           'tradeMethods', 'fiatSymbol',
+            'nickName', 'monthOrderCount', 'monthFinishRate', 'positiveRate','tradeMethodName'
+      ]
+      const extractedData = getKeysOnDemand(data, keysToKeep)
+      return {
+        success: true,
+        data: extractedData,
+        message: 'Successfully'
+    }
+  } catch (error) {
+      console.log('error :>> ', error);
+    console.error('Error calling Binance P2P API:', error.message);
+    return {
+      success: false,
+      data: null,
+      message: error.message
+  }
+  }
+}
+
+module.exports = {
+  getTransactions
+};
